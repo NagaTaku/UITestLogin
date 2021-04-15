6ec7b8faaaa39f7ac1a82658248e175a2be379a3
NagaTaku <>
2021-04-15 14:40:53 +0900

    frist commit

---
+++ UITestLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITestLogin
+//
+//  Created by 永瀬拓也 on 2021/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
